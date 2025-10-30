6b5112ddb1e647f3dcb2a543dec40870517e7ae1
Prem Suthar <premsuthar8732@gmail.com>
2025-10-30 14:14:20 +0530

    Initial commit - MyFirstWebApp complete project

--- obj/Debug/net9.0/MyFirstWebApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyFirstWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1ffed0eb8e02dc21108ad232b9db107bcffab72")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
